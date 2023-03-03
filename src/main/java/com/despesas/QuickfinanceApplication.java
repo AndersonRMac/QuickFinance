f2a5f22c59019df8c25ab2241490fdf1d1da8cca
Anderson R Maciel <90875882+AndersonRMac@users.noreply.github.com>
2023-03-03 16:54:09 -0300

    Add files via upload

---
+++ src/main/java/com/despesas/QuickfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/despesas/QuickfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
